a7f6c6f71a1af40ad9f0b26bbe750ad3332331d2
吴国宏 <303295469@qq.com>
2020-06-06 15:06:47 +0800

    新增java代码

--- src/HuaWeiTestCode.java
+++ src/HuaWeiTestCode.java
@@ -9,13 +9,17 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.regex.Pattern;
 
+/**
+ *
+ */
 public class HuaWeiTestCode {
     public static void main(String[] args) {
-        danCiYasuoBianMa();
+        int result = lengthOfLongestSubstring();
+        System.out.println(result);
     }
 
     /**
-     * 九宫格
+     * 【九宫格】
      * 判断输出，有英文和数字两个模式，
      * 默认是数字模式，数字模式直接输出数 字，
      * 英文模式连续按同一个按键会依次出现这个按键上的字母，
@@ -82,9 +86,12 @@ public class HuaWeiTestCode {
 
     }
 
-     /**
-     * 相加
-     * s=a+aa+aaa+aaaa+aa…a(总共有n个这样的数相加)的值
+    /**
+     * 【相加】华老师让你们计算一个等式，s=a+aa+aaa+aaaa+aa…a(总共有n个这样的数相加)的值，其中a是一个1～9 的数字。
+     * 例如当a ＝ 2，n ＝ 5时，s ＝ 2+22+222+2222+22222。
+     * 输入描述：输入一行包含两个整数a(1<=a<=9)和n(1<=n<=9)，
+     * 其中n为几个这样的数字相加（如描述中的例子n是5）。a和n用空格隔开
+     *
      * 例如当a ＝ 2，n ＝ 5时，s ＝ 2+22+222+2222+22222
      * 输出结果s
      */
@@ -104,7 +111,7 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 寻找最长字符串
+     * 【寻找最长字符串】
      * 给定一串字符，里面有些字符有连续出现的特点，
      * 请寻找这些连续出现字符中最长的串，如果 最长的字串有多个，
      * 请输出字符ASCII码最小的那一串。
@@ -138,7 +145,8 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 找出不同字母
+     * 【找出不同的字母】给定两个字符串 s 和 t，它们只包含小写字母。 字符串 t 由字符串 s 随机重排，然后在随机位置添加一个字母。 请找出在 t 中被添加的字母。
+     * 输入描述： 输出描述： 示例1： 输入 考生信息 考生成绩 知识点技能图谱 其他知识点 1/1 编码能力 第一行输入源字符串s，第二行输入目标字符串t 找到新添加的字符，并输出
      * 输入
      * Abc
      * bAdc
@@ -165,7 +173,12 @@ public class HuaWeiTestCode {
     //region 字符串处理
 
     /**
-     * 字符串处理
+     * 【字符串处理】
+     * 让我们来玩个字符消除游戏吧, 给定一个只包含大写字母的字符串s，消除过程是如下进行的： 1)如果s包含长度为2的由相同字母组成的子串，那么这些子串会被消除，
+     * 余下的子串拼成新的字符串。 例如”ABCCBCCCAA”中”CC”,”CC”和”AA”会被同时消除，余下”AB”, “C”和”B”拼成新的字符串”ABBC”。 2)
+     * 上述消除会反复一轮一轮进行，直到新的字符串不包含相邻的相同字符为止。 例如”ABCCBCCCAA”经过一轮消除得到”ABBC”，
+     * 再经过一轮消除得到”AC” 输入描述：输入由大写字母组成的字符串s，长度不超过100.  输
+     * 出描述：若最后可以把整个字符串全部消除, 就输出 Yes, 否则输出 No.
      * 演示
      * 输入
      * ABCCBA
@@ -205,7 +218,20 @@ public class HuaWeiTestCode {
     //endregion
 
     /**
-     * 航班预定座位数(使用差分序列做法  O(n),贼快)
+     * 【航班预定座位数】(使用差分序列做法  O(n),贼快)
+     * 这里有 n 个航班，它们分别从 1 到 n 进行编号。
+     * <p>
+     * 我们这儿有一份航班预订表，表中第 i 条预订记录 bookings[i] = [i, j, k] 意味着我们在从 i 到 j 的每个航班上预订了 k 个座位。
+     * 请你返回一个长度为 n 的数组 answer，按航班编号顺序返回每个航班上预订的座位数
+     * 输入：bookings = [[1,2,10],[2,3,20],[2,5,25]], n = 5
+     * 输出：[10,55,45,25,25]
+     * 思路:
+     * 初始航班预订数量数组 answer = [0,0,0,0,0]，差分数组d = [0,0,0,0,0]
+     * 当遍历到bookings[0] = [1,2,10]的时候，差分数组第1位加10，第3位减10，变成d = [10,0,-10,0,0]
+     * 同理，当遍历到bookings[1] = [2,3,20]的时候，差分数组变成d = [10,20,-10,-20,0]
+     * 当遍历到bookings[2] = [2,5,25]的时候，差分数组变成d = [10,45,-10,-20,0]，第6位要减25，我们也不需要了
+     * 最后计算answer数组的值，answer[0] = d[0] = 10，answer[1] = d[1] + answer[0] = 45 + 10 = 55，answer[2] = d[2] + answer[1] = -10 + 55 = 45…
+     * 最最后发现，只申请一个数组表示d[]和answer[]就可以了，over！
      *
      * @param bookings 预定信息 [i,j,k] 代表从i号到j号有k人订票
      * @param n        代表一共几个航班
@@ -236,9 +262,12 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 奥运会排行榜
+     * 【奥运会排行榜】
+     * 2012伦敦奥运会即将到来，大家都非常关注奖牌榜的情况，现在我们假设奖牌榜的排名规则如下： 1、首先gold med al数量多的排在前面；
+     * 2、其次silver medal数量多的排在前面；
+     * 3、然后bronze medal数量多的排在前面；
+     * 4、若以上三个条件仍无法区分名次，则以国家名称的 字典序排定。 我们假设国家名称不超过20个字符、各种奖牌数不超过100，且大于等于0
      * 示例
-     * 5
      * China 32 28 34
      * England 12 34 22
      * France 23 33 2
@@ -298,7 +327,7 @@ public class HuaWeiTestCode {
     //region 字符串分隔
 
     /**
-     * 字符串分割
+     * 【字符串分割】
      * 给定一个非空字符串S，其被N个'-'分隔成N+1的子串，给定正整数K，
      * 要求除第一个子串外，其余的 串每K个用'-'分隔，并将小写字母转换为大写。
      */
@@ -336,7 +365,7 @@ public class HuaWeiTestCode {
     //endregion
 
     /**
-     * 猜数字游戏
+     * 【猜数字游戏】
      * 10.xAyB 猜数字游戏，输入两组数字，每组数字包含4个非负整型数字，
      * 同一组中的4个数字互不相 同，数字间以空格分隔。
      * 第一组数字为猜数字游戏的正确答案，第二组数字为玩家所猜的答案，
@@ -370,8 +399,11 @@ public class HuaWeiTestCode {
         //注意这种写法没有的时候会输出 0A0B
     }
 
-    //region 服务器广播问题
-
+    /**
+     * 【服务器广播问题】服务器连接方式包括直接相连，间接连接。A和B直接连接，B和C直接连接，则A和C间接连接。直接连接和间接连接 都可以发送广播。
+     * 给出一个N*N数组，代表N个服务器，matrix[i][j] == 1，则代表i和j直接连接；不等于1时，代表i和j不直接连接。matrix[i][i] == 1，
+     * 即自己和自己直接连接。Matrix[i][j]==matrix[j][i]计算初始需要给几台服务器广播，才可以使每个服务器都收到广播。
+     */
     /**
      * @param match 一维和二维分辨表示第几号和第几号相连，值1为直接，0为不直接
      */
@@ -400,7 +432,7 @@ public class HuaWeiTestCode {
     //endregion
 
     /**
-     * 重新输出句子
+     * 【新输出句子】
      * 输入一个英文句子，句子中仅包含英文字母，数字，
      * 空格和标点符号，其中数字、空格和标点符号将句子划分成一个 个独立的单词
      * ，除去句子中的数字、空格和标点符号，将句子中的每个单词的首字母大写，然
@@ -425,7 +457,8 @@ public class HuaWeiTestCode {
     //region 字符串匹配
 
     /**
-     * 字符串匹配 b>=a
+     * 【字符串匹配 】b>=a
+     * 14.给你两个字符串 a 和 b ，要求输出从 b 中能找出多少和 a 相同的子串，允许有部分重叠。
      */
     static void stringMatch() {
         Scanner sc = new Scanner(System.in);
@@ -459,7 +492,9 @@ public class HuaWeiTestCode {
 
     /**
      * 全排列算法
-     * 输入1,2,3,4
+     * 给定一个正整数n，我们取出前n小的正整数，即1~n这n个数字，将他们排列，就一共有n!种排列方 案，
+     * 所有的排列方案统称为n的全排列，现在你需要做的事情就是把n的全排列都输出出来。
+     * 输入描述：输入只有一个数字n(2 <= n <= 7) 输出描述：输出n的全排列。输出n!个数字的一维数组，输出的顺序按照数字从小到大输出
      */
     static void quanPaiLie() {
         Scanner sc = new Scanner(System.in);
@@ -478,7 +513,7 @@ public class HuaWeiTestCode {
             return;
         }
         for (int i = 0; i < array.length; i++) {
-            int[] tempArray = new int[array.length - 1];//还有两个可选择，给递归
+            int[] tempArray = new int[array.length - 1];//还有剩下的可选择，给递归
             System.arraycopy(array, 0, tempArray, 0, i);//把i之前元素放进数组
             System.arraycopy(array, i + 1, tempArray, i, array.length - i - 1);//把i之后元素放进数组
             stack.push(array[i]);
@@ -489,7 +524,7 @@ public class HuaWeiTestCode {
     //endregion
 
     /**
-     * 计算立方和
+     * 【计算立方和】
      */
     static void jiSuanLiFangHe() {
         Scanner sc = new Scanner(System.in);
@@ -505,7 +540,7 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 判断一组数字是否连续
+     * 【判断一组数字是否连续】
      * 判断一组数字是否连续 当出现连续数字的时候以‘-’输出，比如：
      * 对于数组[1, 2, 3, 4, 6, 8, 9, 10]，期望输出：["1-4", 6, "8-10"]
      */
@@ -544,7 +579,9 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 报数游戏
+     * 【报数游戏】
+     * 19.100个人围成一圈，每个人有一个编码，编号从1开始到100。他们从1开始依次报数，报到为M的人自动退出圈圈，然 后下一个人接着从1开始报
+     * 数，直到剩余的人数小于M。请问最后剩余的人在原先的编号为多少？ 例如输入M=3时，输出为： “58,91” ，输入M=4时，输出为： “34,45,97”。
      */
     static void reportNumGame() {
         Scanner sc = new Scanner(System.in);
@@ -576,7 +613,10 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 正数翻转
+     * 【正数翻转】
+     * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。输出结果超出有效范围的输入，则返回0
+     * 输入描述：输入为符合要求的整数，无需校验
+     * 输出描述：输出翻转后的整数，即一个32位的有符号整数，超出范围返回0
      */
     static void zhengShuFanZhuan() {
         Scanner sc = new Scanner(System.in);
@@ -605,7 +645,10 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 幸运数字
+     * 【幸运数字】
+     * 我们这么定义幸运数：若k能被x、y、z任意一个数整除就可认为k是幸运数。现在你的任务是计算区间 [1,n]之间一共有多少个幸运数。
+     * 输入描述：输入只有一行，包含四个整数n,x,y,z(1<= 10^9,1< x,y,z
+     * 输出描述：输出只有一行，即幸运数的个数
      */
     static void xingYunShuZi() {
         Scanner sc = new Scanner(System.in);
@@ -625,7 +668,7 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 分子弹
+     * 【分子弹】
      * 射击训练，需要给每个士兵发子弹，
      * 发子弹的个数规则是根据士兵的训练成绩给定的，
      * 传入士兵的训练成 绩，要求相邻士兵中，成绩好的士兵的子弹数必须更多，
@@ -653,7 +696,7 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 元音字母请大写
+     * 【元音字母请大写】
      * solo从小就对英文字母非常感兴趣，尤其是元音字母(a,e,i,o,u,A,E,I,O,U)，他在写日记的时候都会把元音字母写成大 写的，
      * 辅音字母则都写成小写，虽然别人看起来很别扭，但是solo却非常熟练。
      * 你试试把一个句子翻译成solo写日记的习惯吧。
@@ -674,10 +717,13 @@ public class HuaWeiTestCode {
         System.out.println(sb);
     }
 
-    //region 跳跃比赛
 
     /**
-     * 跳跃比赛
+     * 【跳跃比赛】
+     * 25.给出一组正整数，你从第一个数向最后一个数方向跳跃，每次至少跳跃1格，每个数的值表示你从这个 位置可以跳跃的最大长度。计算如何以最少的跳跃次数跳到最后一个数。
+     * <p>
+     * 输入描述：第一行表示有多少个数n 第二行开始依次是1到n个数，一个数一行
+     * 输出描述：输出一行，表示最少跳跃的次数。
      */
     static void tiaoYueBiSai() {
         Scanner sc = new Scanner(System.in);
@@ -704,7 +750,7 @@ public class HuaWeiTestCode {
     //endregion
 
     /**
-     * 单词反转
+     * 【单词反转】
      */
     static void danCiFanZhuan() {
         Scanner sc = new Scanner(System.in);
@@ -723,48 +769,49 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 无重复字符的最长字串
+     * 【无重复字符的最长字串】
      * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度
      * 输出不含有重复字符的最长子串的长度
      */
-    //双重指针写法!!!!
+    //双重指针写法!!!!,aaacccc
     static int lengthOfLongestSubstring() {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         int[] hash = new int[500];
         int startIndex = 0;
         int max = 0;
-        int i = 0, j = 0;
-
-        while (i < s.length() && j < s.length()) {
-            char iCh = s.charAt(i);
-            char jCh = s.charAt(j);
-            if (hash[s.charAt(j)] == 0) {
-                hash[s.charAt(j)] = 1;
-                j++;
-                // max = (j - i) > max ? (j - i) : max;
-                // 这里优化一下 输出最长字符串
-                if (j - i > max) {
+        int start = 0, end = 0;
+        while (end < s.length()) {
+            char startCh = s.charAt(start);
+            char endCh = s.charAt(end);//拿到Ascii码
+            if (hash[endCh] == 1) {//代表字符连续
+                end++;//
+                if (end - start > max) {
                     //说明后面的比前面要长了，
-                    max = j - i;
-                    //从i开始，i不是索引是长度
-                    startIndex = i;
+                    max = end - start;
+                    startIndex = start;
                 }
 
-            } else {
-                hash[s.charAt(i)] = 0;
-                i++;
+            } else {//代表该字符不连续
+                hash[startCh] = 0;
+                hash[endCh] = 1;
+                start = end;
             }
         }
         //先输出最长子串
-        System.out.println(s.substring(startIndex, max + 1));
+        System.out.println(s.substring(startIndex, startIndex + max));
         return max;
     }
 
     /**
-     * 集五福
+     * 【集五福】
+     * 集五福作为近年来大家喜闻乐见迎新春活动，集合爱国福、富强福、和谐福、友善福、敬业福即可分享超大红包 题目：
+     * 以0和1组成的长度为5的字符串代表每个人所得到的福卡，每一位代表一种福卡，1表示已经获得该福卡，单类 型福卡不超过1张，
+     * 随机抽取一个小于10人团队，求该团队最多可以集齐多少套五福
+     * 输入描述：输入指定个数类似11010,00110，由0、1组成的长度小于5的字符串，代表指定团队中每个人福卡获得情况
+     * 输出描述：输出该团队能凑齐多少套五福
      */
-    static void jiWuFu(){
+    static void jiWuFu() {
         Random r = new Random();
         int ran;
         while ((ran = r.nextInt(10)) != 0) {
@@ -775,9 +822,9 @@ public class HuaWeiTestCode {
                 char[] chs = input.toCharArray();
                 for (int j = 0; j < chs.length; j++) {
                     if (map.get(j) != null) {
-                        map.put(j, map.get(j) +(chs[j]=='0'?0:1));
+                        map.put(j, map.get(j) + (chs[j] == '0' ? 0 : 1));
                     } else {
-                        map.put(j, (chs[j]=='0'?0:1));
+                        map.put(j, (chs[j] == '0' ? 0 : 1));
                     }
                 }
             }
@@ -789,6 +836,13 @@ public class HuaWeiTestCode {
 
     /**
      * 磁盘容量排序
+     * 30.磁盘的容量单位常用的有有M，G，T这三个等级，它们之间的换算关系是1T = 1000G，1G = 1000M，
+     * 现在给定n块磁盘的容量，请对它们按从小 到大的顺序进行排序，例如给定3块盘的容量，1T，20M，3G，排序后的结果未20M，3G，1T
+     * 输入描述：
+     * 1、每个测试用例第一行包含一个整数n(2 <= n <= 1000)，表示磁盘的个数，接下的n行，每行一个字符串（长度大于2，小于10），表示磁盘的容量，格式为：mv ，其中m表示容量大小，v表示容量单位，例如20M，1T，30G
+     * 2、磁盘容量m表示十进制数范围为1到1000的正整数，容量单位v的范围只包含题目中提到的M，G，T三种，换算关系如题目描述
+     * 输出描述：
+     * 输出n行，表示n块磁盘容量排序后的结果
      */
     static void ciPanRoliangPaiXu() {
         Scanner sc = new Scanner(System.in);
@@ -825,8 +879,8 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 括号生成
-     * 31.给定一个无重复元素的数组 和一个目标数 target ，
+     * 【括号生成】
+     * 给定一个无重复元素的数组 和一个目标数 target ，
      * 找出 数组中所有可以使数字和为 target 的组合。数组中的数字可以无限制被选取。
      * 说明： 所有数字（包括 target）都是正整数。 解集不能包含重复的组合
      */
@@ -839,14 +893,6 @@ public class HuaWeiTestCode {
         if (sum == target) {
             System.out.println(stack.toString());
         }
-//        for (int value:array
-//             ) {
-//            if(sum+value<=target){
-//                stack.push(value);
-//                kuoHaoShengCheng(array,,target,stack);
-//                stack.pop();
-//            }
-//        }
         for (int i = startIndex; i < array.length; i++) {
             if (sum + array[i] <= target) {
                 stack.push(array[i]);
@@ -857,7 +903,10 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 连续字母
+     * 【连续字母】
+     * 给你一个字符串，只包含大写字母，求同一字母连续出现的最大次数。例如”AAAABBCDHHH”，
+     * 同一字 母连续出现的最大次数为4，因为一开始A连续出现了4次。 输入描述：输入一个子串(1<长度<=100)。
+     *          输出描述：输出对应每个子串同一字母连续出现的最大次数。
      */
     static void lianXuZiMu() {
         Scanner sc = new Scanner(System.in);
@@ -878,7 +927,9 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 乘最多水的容器
+     * 【乘最多水的容器】
+     * 给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。
+     * 在坐标内画 n 条垂直 线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水
      */
     static void zuiDuoShuiRongQi(int[] array) {
         int left = 0;
@@ -932,15 +983,17 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 单词压缩编码
+     *【 单词压缩编码】
+     * 给定一个单词列表，我们将这个列表编码成一个索引字符串 S 与一个索引列表 A。 例如，如果这个列表是 ["time", "me", "bell"]，我们就可以将其表示为 S = "time#bell#" 和 indexes = [0, 2, 5]。 对于每一个索引，我们可以通过从字符串 S 中索引的位置开始读取字符串，
+     * 直到 "#" 结束，来恢复我们之前的单词列 表。 那么成功对给定单词列表进行编码的最小字符串长度是多少呢？输入描述：输入: words = ["time","me", "bell"] 输出: 10 说明: S = "time#bell#"，indexes = [0, 2, 5]。输出描述：编码长度
      */
     static void danCiYasuoBianMa() {
         String[] inputs = new String[]{"time", "me", "bell"};
         StringBuilder sb = new StringBuilder();
-        for (String str:inputs
-             ) {
-            if(sb.indexOf(str+"#")==-1){
-                sb.append(str+"#");
+        for (String str : inputs
+        ) {
+            if (sb.indexOf(str + "#") == -1) {
+                sb.append(str + "#");
             }
         }
         System.out.println(sb.length());
@@ -965,7 +1018,7 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 最长公共子串长度
+     * 【最长公共子串长度】
      * 请根据输入两个字符串，求它们的最长公共子串，输出最长公共子串的长度
      * 用动态规划来做求dp
      */
@@ -1011,7 +1064,10 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 字符统计及重排
+     * 【字符统计及重排】
+     * 给出一个仅包含字母的字符串，不包含空格，统
+     * 计字符串中各个字母（区分大小写）出现的次 数，并按照字母出现
+     * 次数从大到小的顺序输出各个字母及其出现次数 信息，如果次数相同，按照自然顺序进行排序。
      */
     static void ziFuTongJiChongPai() {
         Scanner sc = new Scanner(System.in);
@@ -1039,45 +1095,47 @@ public class HuaWeiTestCode {
     }
 
     /**
-     * 字符串数组压缩
+     * 【字符串数组压缩】
+     * 对一个字符串数组进行压缩，保留头部和尾部字符，中间填写省略的长度，即internet可以缩写 为i6t。当两个字符串缩写一致时，增加头部保留的字符数，
+     * 例如：aabbcc和abbbcc可以缩写为aa3c和ab3c。如果缩 写之后的长度不小于原始的长度，则保留原始的字符串，不进行缩写
      */
     static void stringArrayZip(String[] strList) {
-        Map<String,String> set = new HashMap<>();
+        Map<String, String> set = new HashMap<>();
         for (String str : strList
         ) {
             StringBuilder sb = new StringBuilder();
             sb.append(str.substring(0, 1));
             sb.append(str.length() - 2);
             sb.append(str.substring(str.length() - 1, str.length()));
-            int count =1;
+            int count = 1;
             while (set.containsKey(sb.toString())) {
                 count++;
                 String getStr = set.get(sb.toString());
                 set.remove(sb.toString());
                 StringBuilder tempSb = new StringBuilder();
-                tempSb.append(getStr.substring(0,count));
-                tempSb.append(getStr.length()-count-1);
-                tempSb.append(getStr.substring(getStr.length()-1,getStr.length()));
-                set.put(tempSb.toString(),getStr);
+                tempSb.append(getStr.substring(0, count));
+                tempSb.append(getStr.length() - count - 1);
+                tempSb.append(getStr.substring(getStr.length() - 1, getStr.length()));
+                set.put(tempSb.toString(), getStr);
 
                 sb.delete(0, sb.length());//删除之后新增
-                sb.append(str.substring(0,count));
-                sb.append(str.length()-count-1);
-                sb.append(str.substring(str.length()-1,str.length()));
+                sb.append(str.substring(0, count));
+                sb.append(str.length() - count - 1);
+                sb.append(str.substring(str.length() - 1, str.length()));
             }
             //跳出循环，就是不存在了,判断长度
-            if(sb.length()>=str.length()){
-                sb.delete(0,sb.length());
+            if (sb.length() >= str.length()) {
+                sb.delete(0, sb.length());
                 sb.append(str);
             }
-            set.put(sb.toString(),str);
+            set.put(sb.toString(), str);
         }
-        set.forEach((k,v)-> System.out.println(k));
+        set.forEach((k, v) -> System.out.println(k));
     }
 
-
-
-
+    /**
+     * 以下非华为测试题，但也可参考
+     */
 
     static void fileStreamTestMethod() {
         try {
@@ -1157,6 +1215,13 @@ public class HuaWeiTestCode {
         }
     }
 
+    /**
+     * 分苹果问题
+     * m个苹果分给n个小朋友，有几种分法
+     * @param m
+     * @param n
+     * @return
+     */
     static int divideApple(int m, int n) {
         if (n == 1 || m == 0) return 1;
         if (m < n) {
